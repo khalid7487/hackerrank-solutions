e04ccd9cd74ab23c9b58d8a9dc19a96c1875abf5
khalid7487 <khalid15-7487@diu.edu.bd>
2022-06-19 20:20:50 +0600

    jumping on the clouds

---
+++ 25-Jumping on the Clouds/jumping-on-the-clouds.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+    let result =0;
+    
+    for(let i=0; i< c.length-1;  i++){
+        
+        if(c[i] == 0){
+            console.log(c[i]);
+            i++;
+        }    
+          result ++;  
+        
+    }
+    
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
---
+++ 25-Jumping on the Clouds/jumping-on-the-clouds.md
@@ -0,0 +1,7 @@
+**Link** https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+
+` Here we need to calculate the total number of jump` 
+## Algorithms 
+1. loop through 0...n .
+2. if c[i] === 0 if i++
+4. result++
