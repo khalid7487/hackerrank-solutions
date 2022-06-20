6fcfe3d6dc32161b4265ec1225ade2da22c5bfa3
khalid7487 <khalid15-7487@diu.edu.bd>
2022-06-20 20:18:05 +0600

    cut the sticks

---
+++ 27-Cut the sticks/Cutthesticks.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'cutTheSticks' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function cutTheSticks(arr) {
+    // Write your code here
+    let resultArray = [];
+    let n = arr.length;
+
+    let copyArray = arr;
+
+    while (n > 0) {
+        let count = 0;
+        copyArray = copyArray.filter(val => val > 0);
+        
+        let minValue = Math.min.apply(null, copyArray);
+        let k = copyArray.length;
+        for (let i = 0; i < k; i++) {
+            let copyValue = copyArray[i] - minValue;
+            let j= i;
+            copyArray.fill(copyValue, i, j+1);
+            count++;
+        }
+        resultArray.push(count);
+        n--;
+    }
+
+    resultArray = resultArray.filter(val => val>0);
+    
+    return resultArray;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = cutTheSticks(arr);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
