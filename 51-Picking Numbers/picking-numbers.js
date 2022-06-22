475a4a385a67a8e9d18379436040e0d61925a2ba
khalid7487 <khalid15-7487@diu.edu.bd>
2022-06-22 22:27:54 +0600

    picking numbers

---
+++ 51-Picking Numbers/picking-numbers.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    // Write your code here
+
+    let sorted = a.sort(function (a, b) {
+        return a - b;
+    });
+
+    let n = sorted.length
+    let i = 0, j = 1;
+
+    while (j < n) {
+        if (Math.abs(sorted[i] - sorted[j]) > 1) {
+            i++;
+            j++;
+        } else {
+            j++;
+        }
+    }
+
+    let result = Math.abs(i - j);
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
